91c6e8e08aee07b399dcd04fec4c9adac01b85e9
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2025-06-14 23:42:43 +0530

    feat: added UI/UX changes

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ const App = () => (
           <Route path="/about" element={<About />} />
           <Route path="/onboarding" element={<Onboarding />} />
           <Route path="/integrations" element={<Integrations />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
